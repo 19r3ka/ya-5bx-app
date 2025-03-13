b12f3e56e55dab21a772b42a0342550b31d8d430
19r3ka <yemahiatsi@gmail.com>
2025-03-13 22:46:27 +0000

    feat: Configure base path for GitHub Pages and improve TypeScript support
    Configured Vite's base path for proper asset loading on GitHub Pages.
    Added custom type definitions for Vuetify and modified tsconfig.
    Updated the deployment workflow.

--- eslint.config.js
+++ eslint.config.js
@@ -7,7 +7,15 @@ import stylistic from '@stylistic/eslint-plugin'
 
 /** @type {import('eslint').Linter.Config[]} */
 export default [
-  { files: ['**/*.{js,mjs,cjs,ts,vue}'] },
+  {
+    files: ['**/*.{js,mjs,cjs,ts,vue}'],
+    ignores: [
+      '**/node_modules/**', // Ignore node_modules
+      '**/dist/**', // Ignore dist folder
+      '**/coverage/**', // ignore coverage folder
+      // Add other paths or file patterns you want to ignore here
+    ],
+  },
   { languageOptions: { globals: globals.browser } },
   pluginJs.configs.recommended,
   ...tseslint.configs.recommended,
