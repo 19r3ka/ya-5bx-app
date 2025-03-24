21e62fd374ca58bd13766785a20e94638e5c7cb2
19r3ka <yemahiatsi@gmail.com>
2025-03-24 07:47:11 +0000

    feat: Implement onboarding logic including getting user name and age to evaluate start and goal chart levels.

--- src/constants.ts
+++ src/constants.ts
@@ -1,4 +1,4 @@
-export const EXERCISE_TIMES = [2, 1, 1, 1, 6]
+export const EXERCISE_TIMES = [120, 60, 60, 60, 360]
 // exercise 6 has a mix of cardio (75 reps) and plyometric (10 reps) exercises
 export const EXERCISE_5_REPS = [75, 10]
 export const EXERCISE_LEVELS = [
@@ -15,7 +15,10 @@ export const EXERCISE_LEVELS = [
   'A',
   'A+',
 ]
+export const LOCAL_STORAGE_TIMER_KEY = 'ya-5bx_timer'
 export const LOCALSTORAGE_USER_KEY = 'ya-5bx_user'
+export const LOCAL_STORAGE_WORKOUT_KEY = 'ya-5bx_workout'
+
 export const SECONDS_IN_A_YEAR = 31536000 // 365 days * 24 hours * 60 minutes * 60 seconds
 export const STARTER_LEVEL = 'C1_D-'
 export const MINIMUM_REQUIRED_AGE = 6
